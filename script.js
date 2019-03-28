670448f7b62b1c045fcba68611b03a95df402134
JoannaGlezz <zayra.2424@gmail.com>
2019-03-27 22:21:48 -0600

    html listo

---
+++ script.js
@@ -0,0 +1,17 @@
+let miAuto = []
+
+function crear(e) {
+
+    let marca = document.querySelector("form")
+    console.log(marca)
+
+    let auto = {
+        marca: "",
+        modelo: "",
+        color: "",
+        refaccion: "",
+        puertas: "",
+        versiones: ""
+    }
+}
+
