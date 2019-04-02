bb7f53cb86f304fb1f74f9dab329a168cc092589
JoannaGlezz <zayra.2424@gmail.com>
2019-04-02 10:27:43 -0600

    cambios

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-let miAuto = []
-
-function crear(e) {
-
-    let marca = document.querySelector("form")
-    console.log(marca)
-
+function crear() {
+    const valores = getValueObjectFromForm("formMarca")
+    console.log('El valor es', valores)
+    
     let auto = {
-        marca: "",
-        modelo: "",
-        color: "",
-        refaccion: "",
-        puertas: "",
-        versiones: ""
+        marca: valores.marca,
+        modelo: valores.modelo,
+        color: valores.color,
+        llanta: valores.llanta ,
+        puertas: valores.puertas,
+        versiones: valores.version
     }
+    console.log('mi auto es:', auto)
 }
 
+function getValueObjectFromForm(groupId) {
+    var form = document.getElementById(groupId);
+
+    var data = new FormData(form);
+    var output = {};
+    for (const entry of data) {
+        output[entry[0]] = entry[1]
+    };
+    return output
+}
+    
